74b60d9ddc851f4795aa969d615d588cc388f0ec
ilia24 <iliademertchian24@gmail.com>
2017-03-29 15:04:47 -0400

    Finish exercise 1

--- bankaccount.rb
+++ bankaccount.rb
@@ -1,4 +1,18 @@
 class BankAccount
   attr_accessor :balance
   attr_accessor :interest_rate
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+
+  def gain_interest(percent)
+    floatpercent = percent.to_f
+    fixpercent = ((floatpercent / 100) + 1)
+    @balance = @balance * fixpercent
+  end
 end
