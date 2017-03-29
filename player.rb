7c8af165c863d0b03ab968a9a78a1997a878de01
ilia24 <iliademertchian24@gmail.com>
2017-03-29 17:43:48 -0400

    Add treasure collect method

--- player.rb
+++ player.rb
@@ -8,4 +8,19 @@ class Player
   def level_up
     @lives += 1
   end
+
+  def collect_treasure
+    @gold_coins += 1
+    levels = 0
+
+    if @gold_coins % 10 == 0
+      levels += 1
+    end
+
+    if levels > 0
+      @lives += 1
+      levels -= 1
+      puts "Level up!"
+    end
+  end
 end
